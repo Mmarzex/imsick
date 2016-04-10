c4176fe5ef5c2b153f426add80c1053a3bf660b6
John Walter Stockwell <stocklett@gmail.com>
2016-04-10 13:24:46 -0400

    Updated the default data values

--- server/main.js
+++ server/main.js
@@ -3,8 +3,6 @@ import '../imports/api/Reports';
 
 import {Reports} from '../imports/api/Reports';
 
-const faker = require('faker');
-
 const coords = [
     [40.804037, -77.865789],
 [40.803443, -77.865738],
@@ -29,4 +27,53 @@ Meteor.startup(() => {
   //       newReport.symptoms.push(symptoms[randomSymptom]);
   //       Reports.insert(newReport);
   //   }
+  
+  // Sicknesses
+  var sepsis = ["Fever","Coughing","Stomach Ache"];
+  var syphilis = ["Sore Throat","Post Nasal Drip","Headache","Congestion"];
+  var poops = ["Children","Unsatisfying Sex Life","Adulthood","Mortgage"];
+  var cancer = ["Cancer","Jaundis","Gangrene","Scurvy", "White Whale"];
+  var dead = ["Dead","Dead","Dead","Dead"];
+  
+  //40.802690, -77.867689
+  Reports.insert({lat:40.802690, lng:-77.867689, symptoms:sepsis});
+  //40.802568, -77.867776
+  Reports.insert({lat:40.802690, lng:-77.867689, symptoms:poops});
+  //40.802475, -77.867902
+  Reports.insert({lat:40.802475, lng:-77.867902, symptoms:syphilis});
+  //40.802345, -77.867996
+  Reports.insert({lat:40.802345, lng:-77.867996, symptoms:sepsis});
+  //40.802075, -77.868206
+  Reports.insert({lat:40.802075, lng:-77.868206, symptoms:syphilis});
+  //40.802630, -77.868835
+  Reports.insert({lat:40.802630, lng:-77.868835, symptoms:poops});
+  //40.803028, -77.868465
+  Reports.insert({lat:40.803028, lng:-77.868465, symptoms:cancer});
+  //40.802929, -77.869165
+  Reports.insert({lat:40.802929, lng:-77.869165, symptoms:poops});
+  //40.803256, -77.868928
+  Reports.insert({lat:40.803256, lng:-77.868928, symptoms:sepsis});
+  //40.803126, -77.869049
+  Reports.insert({lat:40.803126, lng:-77.869049, symptoms:poops});
+  //40.803098, -77.869019
+  Reports.insert({lat:40.803098, lng:-77.869019, symptoms:dead});
+  //40.803098, -77.869019
+  Reports.insert({lat:40.803098, lng:-77.869019, symptoms:sepsis});
+  //40.803328, -77.869753
+  Reports.insert({lat:40.803328, lng:-77.869753, symptoms:syphilis});
+  //40.803302, -77.869807
+  Reports.insert({lat:40.803302, lng:-77.869807, symptoms:cancer});
+  //40.803122, -77.869928
+  Reports.insert({lat:40.803122, lng:-77.869928, symptoms:syphilis});
+  //40.803549, -77.868637
+  Reports.insert({lat:40.803549, lng:-77.868637, symptoms:dead});
+  //40.803821, -77.869076
+  Reports.insert({lat:40.803821, lng:-77.869076, symptoms:syphilis});
+  //40.803267, -77.868082
+  Reports.insert({lat:40.803267, lng:-77.868082, symptoms:sepsis});
+  //40.803499, -77.865522
+  Reports.insert({lat:40.803499, lng:-77.865522, symptoms:cancer});
+  //40.803555, -77.865615
+  Reports.insert({lat:40.803555, lng:-77.865615, symptoms:dead});
+  
 });
